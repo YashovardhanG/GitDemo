a0d76f19e414eef707528f017a8a2a0f243495ee
YashovardhanG <yashovardhangoyal@gmail.com>
2019-04-30 13:53:07 +0530

    first commit

---
+++ src/Methods.java
@@ -0,0 +1,17 @@
+
+public class Methods {
+
+	public void first () {
+		
+		System.out.println("This is first method");
+		
+	}
+	
+	
+public static void  second() {
+		
+		System.out.println("This is second method");
+		
+	}
+	
+}
---
+++ src/Parent.java
@@ -0,0 +1,15 @@
+
+public class Parent {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is main program");
+		
+		Methods.second();
+		
+		Methods m1 = new Methods();
+		
+        m1.first();
+	}
+
+}
